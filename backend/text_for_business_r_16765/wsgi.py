a77fe26e342887760522a56d9ef2dbe4aa79f583
Crowdbotics <team@crowdbotics.com>
2020-05-08 18:45:50 +0000

    Update models

--- backend/text_for_business_r_16765/wsgi.py
+++ backend/text_for_business_r_16765/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'text_for_business_r_16765.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "text_for_business_r_16765.settings")
 
 application = get_wsgi_application()
